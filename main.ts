868801efe1b470da02a7cfd2ac6de8ff94e27736
felixtsu <humaeks@gmail.com>
2020-03-17 20:15:42 +0800

    1. jsdoc fix

--- main.ts
+++ main.ts
@@ -3,18 +3,31 @@ namespace spritelives {
     let lifeZeroHandlers: { [key: number]: (sprite: Sprite) => void } = {}
 
     /**
-     * 
+     * set sprite lives
+     * @param sprite
+     * @param lives
      */
+    //% lives.defl=-1
     //% blockId="set_sprite_life" block="set %sprite=variables_get(mySprite) life to %lives"
     export function setSpriteLife(sprite: Sprite, lives: number) {
         sprite.data[SPRITE_LIVES_EXTENSION_LIFE_DATA] = lives
     }
 
+    /**
+     * get sprite lives
+     * @param sprite
+     */
     //% blockId="get_sprite_life" block="%sprite=variables_get(mySprite) life"
     export function lifeOf(sprite: Sprite) {
         return sprite.data[SPRITE_LIVES_EXTENSION_LIFE_DATA]
     }
 
+    /**
+     * change sprite lives
+     * @param sprite
+     * @param delta
+     */
+    //% delta.defl=-1
     //% blockId="change_sprite_life" block="change %sprite=variables_get(mySprite) life by $delta"
     export function changeLifeBy(sprite: Sprite, delta: number) {
         let life = lifeOf(sprite)
